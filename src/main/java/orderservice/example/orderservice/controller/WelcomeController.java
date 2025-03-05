ad5a38becc861f3566b524597af32cfbba037c6a
cfkubo <arul.vannala@gmail.com>
2025-03-04 18:13:22 -0700

    update home page

--- src/main/java/orderservice/example/orderservice/controller/WelcomeController.java
+++ src/main/java/orderservice/example/orderservice/controller/WelcomeController.java
@@ -22,6 +22,36 @@ public class WelcomeController {
         sb.append("<li><a href=\"http://localhost:9090/actuator/swagger-ui/index.html?urls.primaryName=x-actuator\" target=\"_blank\">Swagger UI (x-actuator)</a></li>");
 
         sb.append("</ul>");
+
+        // Add sample curl code
+        sb.append("<h2>Sample cURL Commands:</h2>");
+        sb.append("<pre><code>");
+        sb.append("curl -X POST \"http://localhost:8080/api/orders\" \\\n");
+        sb.append("-H \"Content-Type: application/json\" \\\n");
+        sb.append("-d '{\n");
+        sb.append("\"orderId\": 2,\n");
+        sb.append("\"customerName\": \"Sample Order\",\n");
+        sb.append("\"totalAmount\": 11111111\n");
+        sb.append("}'\n");
+        sb.append("\n");
+        sb.append("<br>\n");
+        sb.append("{\n");
+        sb.append("\"id\":2,\n");
+        sb.append("\"customerName\":\"Sample Order\",\n");
+        sb.append("\"totalAmount\":1.1111111E7\n");
+        sb.append("}\n");
+        sb.append("\n");
+        sb.append("<br>\n");
+        sb.append("curl -X POST \"http://localhost:8080/api/orders\" \\\n");
+        sb.append("-H \"Content-Type: application/json\" \\\n");
+        sb.append("-d '{\n");
+        sb.append("\"orderId\": 1,\n");
+        sb.append("\"orderName\": \"Sample Order\",\n");
+        sb.append("\"quantity\": 5,\n");
+        sb.append("\"price\": 19.99\n");
+        sb.append("}'");
+        sb.append("</code></pre>");
+
         return sb.toString();
     }
 }
