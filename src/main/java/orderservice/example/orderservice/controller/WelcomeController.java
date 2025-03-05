8d5e33546cb5229c33952f38dc368857423a55ba
cfkubo <arul.vannala@gmail.com>
2025-03-04 18:10:57 -0700

    update to home links

--- src/main/java/orderservice/example/orderservice/controller/WelcomeController.java
+++ src/main/java/orderservice/example/orderservice/controller/WelcomeController.java
@@ -8,6 +8,20 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Welcome to Order Service!";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<h1>Welcome to Order Service!</h1>");
+        sb.append("<h2>Available Endpoints:</h2>");
+        sb.append("<ul>");
+
+        // Add the static endpoints as clickable links
+        sb.append("<li><a href=\"http://localhost:8080/api/orders\" target=\"_blank\">POST /api/orders</a></li>");
+        sb.append("<li><a href=\"http://localhost:9090/actuator\" target=\"_blank\">GET /actuator</a></li>");
+        sb.append("<li><a href=\"http://localhost:9090/actuator/swagger-ui/index.html#/\" target=\"_blank\">Swagger UI</a></li>");
+        sb.append("<li><a href=\"http://localhost:9090/actuator/openapi/v1\" target=\"_blank\">GET /actuator/openapi/v1</a></li>");
+        sb.append("<li><a href=\"http://localhost:9090/actuator/openapi/x-actuator\" target=\"_blank\">GET /actuator/openapi/x-actuator</a></li>");
+        sb.append("<li><a href=\"http://localhost:9090/actuator/swagger-ui/index.html?urls.primaryName=x-actuator\" target=\"_blank\">Swagger UI (x-actuator)</a></li>");
+
+        sb.append("</ul>");
+        return sb.toString();
     }
 }
